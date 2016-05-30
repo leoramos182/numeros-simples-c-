9335798998489a3df66cf8db54f29f83c20f4fa1
leoramos182 <leo.ramos182@hotmail.com>
2016-05-30 19:57:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um valor!");
+            numero =  int.Parse(Console.ReadLine());
+            Console.WriteLine("Seu numero é "+numero+"!");
+            Console.ReadKey();
+        }
+    }
+}
